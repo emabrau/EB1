9dbb5c31c271e215f2a356574fa0996539d81776
emabrau <144418033+emabrau@users.noreply.github.com>
2023-12-28 16:57:45 +0200

    Update data.h

--- data.h
+++ data.h
@@ -10,11 +10,18 @@ struct Studentas {
     std::vector<double> ndBalai;
     double egzaminas;
     double galutBalas;
+double galutBalasMed;
+double galutBalasVid;
 };
 
 
 void generateAndWriteStudentRecords(const std::string &filename, int size);
 
 void processStudentData(const std::string &filename, int size, int repetitions);
+
+void loadDataFromManualInput(std::vector<Studentas>& studentai);
+void loadData(std::vector<Studentas>& studentai, const std::string& filename);
+void chooseDataInputMethod(std::vector<Studentas>& studentai);
+void generateRandomData(std::vector<Studentas>& studentai, int count);
 
 #endif
