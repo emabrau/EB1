9929363cc3777975260bb8d04a7b945fa023bf8e
emabrau <144418033+emabrau@users.noreply.github.com>
2023-12-28 16:59:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
- #include <iostream>
+#include <iostream>
  #include <fstream>
  #include <string>
  #include <vector>
@@ -7,26 +7,51 @@
  #include <iomanip>
  #include <algorithm>
  #include "data.h"
+#include "functions.h"
+#include <list>
 
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    std::vector<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
-    int repetitions = 5;  
+    char dataChoice;
+    std::cout << "Norite duomenis ivesti ranka (R), nuskaityti is failo (F), ar sugeneruoti atsitiktinius duomenis (G)? ";
+    std::cin >> dataChoice;
 
-    for (int size : recordSizes) {
-        std::string filename = "studentai_" + std::to_string(size) + ".txt";
+    if (dataChoice == 'R' || dataChoice == 'r') {
+        std::vector<Studentas> studentai;
+        loadDataFromManualInput(studentai);
+        GalutinisBalas(studentai);
+        displayTable(studentai);
+    } else if (dataChoice == 'F' || dataChoice == 'f') {
+        std::vector<Studentas> studentai;
+        std::string filename;
+        std::cout << "Iveskite failo pavadinima: ";
+        std::cin >> filename;
+        try {
+            loadData(studentai, filename);
+            GalutinisBalas(studentai);
+            displayTable(studentai);
+        } catch (const std::ifstream::failure& e) {
+            std::cerr << "Error: Nepavyko atidaryti failo." << std::endl;
+            return 1;
+        }
+    } else if (dataChoice == 'G' || dataChoice == 'g') {
+         {
+            std::vector<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
+            int repetitions = 5;
 
-        generateAndWriteStudentRecords(filename, size);
+            for (int size : recordSizes) {
+                std::string filename = "studentai_" + std::to_string(size) + ".txt";
 
-        processStudentData(filename, size, repetitions);
+                generateAndWriteStudentRecords(filename, size);
+                processStudentData(filename, size, repetitions);
+            }
+        
+        } 
     }
 
     return 0;
 }
-
-
-
 
 
 
