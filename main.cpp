f2b2d613f92bb7dafb3cf73233e0564e6b5d39bc
emabrau <144418033+emabrau@users.noreply.github.com>
2023-12-28 16:50:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,32 +5,13 @@
 #include "functions.h"
 #include "data.h"
 #include <stdexcept>
+
 using namespace std;
 
 int main() {
     std::vector<Studentas> studentai; 
-    std::string filename;
 
     try {
-        std::cout << "Norite duomenis ivesti ranka (R) ar nuskaityti is failo (F)? ";
-        char dataChoice;
-        std::cin >> dataChoice;
-
-        if (dataChoice == 'R' || dataChoice == 'r') {
-            loadDataFromManualInput(studentai); 
-        } else if (dataChoice == 'F' || dataChoice == 'f') {
-            std::cout << "Iveskite failo pavadinima: ";
-            std::cin >> filename; 
-          try {
-                loadData(studentai, filename);
-            } catch (const std::ifstream::failure& e) {
-                std::cerr << "Error: Nepavyko atidaryti failo." << std::endl;
-                throw;  
-            }
-        } else {
-            throw std::invalid_argument("Netinkamas pasirinkimas.");
-        }
-
         chooseDataInputMethod(studentai); 
 
         GalutinisBalas(studentai); 
@@ -38,18 +19,15 @@ int main() {
         std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
             return a.vardas < b.vardas;
         });
-      
+
         displayTable(studentai); 
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
+
     return 0;
 }
-
-
-
-
 
 
 
